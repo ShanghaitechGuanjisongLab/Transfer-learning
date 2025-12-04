ac76f31f83667a6027fc5016cf5a1e35011f9b01
埃博拉酱 <vhtmfu@outlook.com>
2025-12-04 16:01:07 +0800

    20251204

--- TransferLearning.m
+++ TransferLearning.m
@@ -15,10 +15,10 @@ classdef(Abstract)TransferLearning
 			MB=UniExp.DataSet("\\Data-Server-2\个人数据\张天夫\202512\MOp全钙.v4.mat");
 			MB.TagSplitTrial(seconds([-3,3]));
 			%由于行为和水混用CD2通道导致多拆出了一个假回合
-			MB.RemoveTrials(MB.TableQuery("TrialUID",DateTime=datetime('2022-08-06 20:26:00',TimeZone='local'),TrialIndex=31).TrialUID);
+			MB.RemoveTrials(MB.TableQuery("TrialUID",DateTime=datetime('2022-08-06 20:26:00'),TrialIndex=31).TrialUID);
 
 			%该日期行为记录了99回合，标通道拆出100回合，每个回合的刺激类型对不上
-			MB.RemoveDateTimes(datetime('2023-01-13 09:39:00',TimeZone='local'));
+			MB.RemoveDateTimes(datetime('2023-01-13 09:39:00'));
 			TrialDuration=seconds(6);
 			LLP=MB.CheckForLightLeakage(seconds([0,0.2]),["LightOnly","LightWater"]);
 			MB.LightLeakageInterpolation(LLP.BlockUID(LLP.Probability>0.95),seconds([0,0.2]),["LightOnly","LightWater"]);
@@ -31,7 +31,7 @@ classdef(Abstract)TransferLearning
 			AL.TagSplitTrial(seconds([-3,3]));
 
 			%该日期行为记录了99回合，标通道拆出100回合，每个回合的刺激类型对不上
-			AL.RemoveDateTimes(datetime('2023-01-13 09:39:00',TimeZone='local'));
+			AL.RemoveDateTimes(datetime('2023-01-13 09:39:00'));
 			TrialDuration=seconds(6);
 			LLP=AL.CheckForLightLeakage(seconds([0,0.2]),["LightOnly","LightWater"]);
 			AL.LightLeakageInterpolation(LLP.BlockUID(LLP.Probability>0.95),seconds([0,0.2]),["LightOnly","LightWater"]);
@@ -41,10 +41,10 @@ classdef(Abstract)TransferLearning
 			LA=UniExp.DataSet("\\Data-Server-2\个人数据\张天夫\202512\光声迁移无穿插MOp成像（含学会后三次）.v3.mat");
 			LA.TagSplitTrial(seconds([-3,3]));
 			%由于行为和水混用CD2通道导致多拆出了一个假回合
-			LA.RemoveTrials(LA.TableQuery("TrialUID",DateTime=datetime('2022-08-06 20:26:00',TimeZone='local'),TrialIndex=31).TrialUID);
+			LA.RemoveTrials(LA.TableQuery("TrialUID",DateTime=datetime('2022-08-06 20:26:00'),TrialIndex=31).TrialUID);
 
 			%该日期行为记录了99回合，标通道拆出100回合，每个回合的刺激类型对不上
-			LA.RemoveDateTimes(datetime('2023-01-13 09:39:00',TimeZone='local'));
+			LA.RemoveDateTimes(datetime('2023-01-13 09:39:00'));
 			TrialDuration=seconds(6);
 			LLP=LA.CheckForLightLeakage(seconds([0,0.2]),["LightOnly","LightWater"]);
 			LA.LightLeakageInterpolation(LLP.BlockUID(LLP.Probability>0.95),seconds([0,0.2]),["LightOnly","LightWater"]);
